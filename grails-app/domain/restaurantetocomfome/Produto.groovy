56bf4fd74c581b9246942b73a193000b1d43fd8e
Bianca <bibca17@hotmail.com>
2016-10-03 17:26:05 -0300

    comentando o hasOne e deixando Estoque estoque

--- grails-app/domain/restaurantetocomfome/Produto.groovy
+++ grails-app/domain/restaurantetocomfome/Produto.groovy
@@ -4,9 +4,10 @@ class Produto {
 
     String nome
     Double preco
+    Estoque estoque
 
-    // hasOne o ID da tabela mãe vai para a tabela filha, ou seja, a referencia da chave estrangeira fica apenas na tabela filha
-    static hasOne = [estoque:Estoque]
+    // hasOne o ID da tabela mãe vai para a tabela filha, ou seja, a referencia da chave estrangeira fica apenas na tabela filha (1-1)
+    // static hasOne = [estoque:Estoque]
 
     static constraints = {
     }
