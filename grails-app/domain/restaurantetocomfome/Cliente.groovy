22a516ab6fd40792dbf49a4fa421c09f809c2e88
Bianca <bibca17@hotmail.com>
2016-10-03 18:02:46 -0300

    acescentado o mapping

--- grails-app/domain/restaurantetocomfome/Cliente.groovy
+++ grails-app/domain/restaurantetocomfome/Cliente.groovy
@@ -7,8 +7,13 @@ class Cliente {
     String senha
 
     // hasMany para relacionamento 1-N, os pedidos podem ser salvos em cascata
-    static hasMany = [ pedidos:Pedido ]
+    static hasMany = [ pedidos:Pedido, produtosPreferidos:Produto ]
 
     static constraints = {
+    }
+
+    static mapping = {
+
+        produtosPreferidos joinTable:[name:"preferencias_clientes", key: "id_cliente", column: "id_produto"]
     }
 }
