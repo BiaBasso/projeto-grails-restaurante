d204c4a6a9197fa23ded5c0138708e62b45bc719
Bianca <bibca17@hotmail.com>
2016-10-03 18:41:37 -0300

    adicionado a hierarquia (extends) com Produto e o mapping

--- grails-app/domain/restaurantetocomfome/Sobremesa.groovy
+++ grails-app/domain/restaurantetocomfome/Sobremesa.groovy
@@ -1,11 +1,16 @@
 package restaurantetocomfome
 
-class Sobremesa {
+class Sobremesa extends Produto{
 
     Integer calorias
 
     static constraints = {
 
         calorias min: 0
+    }
+
+    static mapping = {
+
+        discriminator value: "SOBREMESA"
     }
 }
